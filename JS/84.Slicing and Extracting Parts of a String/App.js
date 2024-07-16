d4c113c6931c0abf1fe19290f79eb8f14b7bf006
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-07-16 16:58:43 +0800

    Update App.js

---
+++ JS/84.Slicing and Extracting Parts of a String/App.js
@@ -0,0 +1,3 @@
+var words = prompt("Slice words");
+var slice = words.slice(0,140);
+alert("characters you've written " + words.length + " characters you've sliced " + slice.length + " characters");
