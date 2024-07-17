6b8af533a8becc1a028596817bc195f49a0ffc27
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-07-17 21:44:26 +0800

    Add files via upload

---
+++ JS/89. Functions Part 1 Creating and Calling Functions/App.js
@@ -0,0 +1,8 @@
+function doBreakfast() {
+    console.log("I getCookie in Refrigirator");
+    console.log("I get also bottle of milk on Refrigirator");
+    console.log("I put the milk into the glass");
+    console.log("I dunk the cookie to the glass of milk");
+    console.log("And I'll eat it");
+}
+doBreakfast();
