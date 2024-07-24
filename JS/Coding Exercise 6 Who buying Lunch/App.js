6d77bc8e20ec9891042307cb8c06fdb74f2f1897
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-07-24 20:04:55 +0800

    Add files via upload

---
+++ JS/Coding Exercise 6 Who buying Lunch/App.js
@@ -0,0 +1,11 @@
+function whosPaying(names) {
+    
+    /******Don't change the code above*******/
+        //Write your code here.
+    var friends = ["Angela", "Ben", "Michael", "Chloe"];
+    var random = Math.floor(Math.random() * friends.length);
+    
+        return friends[random] + " is going to buy lunch today!";
+    
+    /******Don't change the code below*******/    
+    }
