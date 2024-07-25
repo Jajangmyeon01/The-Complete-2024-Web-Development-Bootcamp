c34e5208eedec6114d7c818ac194110707c7f057
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-07-25 21:53:52 +0800

    Add files via upload

---
+++ DOM/121. Manipulating Element Attributes/App.js
@@ -0,0 +1,4 @@
+document.querySelector("a");
+document.querySelector("a").attributes;
+document.querySelector("a").getAttribute("href");
+document.querySelector("a").setAttribute("href", "https://bing.com");
