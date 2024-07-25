e7e781a247cc5c48855e51302cd94ec2466a62bb
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-07-25 21:47:03 +0800

    Add files via upload

---
+++ DOM/120. Text Manipulation and the Text Content/App.js
@@ -0,0 +1,3 @@
+document.getElementById("title").innerHTML = "Good Day!";
+document.getElementById("title").textContent = "Good Afternoon!";
+document.getElementById("title").innerHTML = "<em>Good Day!</em>";
