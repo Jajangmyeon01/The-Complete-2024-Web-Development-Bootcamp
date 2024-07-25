5d28ec2c129685c50caecb91f1672b3c1327d05b
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-07-25 20:20:36 +0800

    Add files via upload

---
+++ DOM/115-116. Solution to the DOM Challenge/App.js
@@ -0,0 +1,5 @@
+document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
+
+var lastList = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
+
+lastList.innerHTML = "Gen Exus C. Vergara";
