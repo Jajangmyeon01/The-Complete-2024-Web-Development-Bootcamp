63d9ffc6ba94e764ada3775afaf1c2355dc31e5c
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-07-25 20:09:24 +0800

    Delete DOM directory

--- DOM/Solution to the DOM Challenge/App.js
+++
@@ -1,5 +0,0 @@
-document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
-
-var lastList = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
-
-lastList.innerHTML = "Gen Exus C. Vergara";
