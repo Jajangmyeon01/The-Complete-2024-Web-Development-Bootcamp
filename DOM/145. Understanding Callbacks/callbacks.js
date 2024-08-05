80d9dae62769fb5eda03e3b131dd55d4ff8cc278
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-08-05 22:56:56 +0800

    Add files via upload

---
+++ DOM/145. Understanding Callbacks/callbacks.js
@@ -0,0 +1,22 @@
+function anotherAddEventListener(typeOfEvent, callback) {
+
+    var eventThatHappened = {
+        eventType: "keypress",
+        key: "p",
+        durationOfKeypress: 2
+    }
+    
+    if (eventThatHappened.eventType === typeOfEvent) {
+        callback(eventThatHappened);
+    }
+    
+    }
+    
+    anotherAddEventListener("keypress", function(event) {
+        console.log(event);
+    });
+    debugger;
+    document.addEventListener("keypress", function(event) {
+        console.log(event);
+    });
+    
