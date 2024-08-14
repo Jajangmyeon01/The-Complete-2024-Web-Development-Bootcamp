626d7750535fd7000fd38bab752d919e9b14501c
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-08-14 01:01:50 -0700

    Add files via upload

---
+++ JQUERY/153. Manipulating Styles with jQuery/index.js
@@ -0,0 +1,9 @@
+
+// console.log($("h1").css("font-size"));
+// $("h1").css("font-size", "5rem");
+// $("h1").removeClass("big-title");
+// $("h1").hasClass("margin-50");
+
+$("h1").addClass("big-title margin-50");
+
+$("button");
