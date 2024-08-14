21f0c0b35f2aeb8ac41555835118523e17887add
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-08-14 05:55:40 -0700

    Add files via upload

---
+++ JQUERY/157. Adding and Removing Elements with jQuery/index.js
@@ -0,0 +1,12 @@
+
+
+// Applying this into console //
+$("h1").before("<button>New</button>");
+
+$("h1").after("<button>New</button>");
+
+$("h1").prepend("<button>New</button>");
+
+$("h1").append("<button>New</button>");
+
+$("button").remove();
