6466134013f1e156f81d20ecfa75a51f5121cec9
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-08-14 06:56:51 -0700

    Add files via upload

---
+++ JQUERY/158. Website Animations with jQuery/index.js
@@ -0,0 +1,15 @@
+
+$("button").on("click", function() {
+
+    // $("h1").show();
+    // $("h1").hide();
+    // $("h1").toggle();
+    // $("h1").fadeOut();
+    // $("h1").fadeIn();
+    // $("h1").fadeToggle();
+    //$("h1").slideUp();
+    //$("h1").slideDown();
+    //$("h1").slideToggle();
+    // $("h1").animate({opacity: 0.5});
+    $("h1").slideUp().slideDown().animate({opacity: 0.5})
+});
