294690f563b2d0c4fbdaa265bd6ef915a12da991
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-10-16 14:10:09 +0800

    Add files via upload

---
+++ REACT/286. React Components/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./components/Heading.jsx";
+import Lists from "./components/lists.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Lists />
+      <Lists />
+    </div>
+  );
+}
+
+export default App;
