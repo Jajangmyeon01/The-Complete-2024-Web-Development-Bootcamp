fb5ab2921087c995bb93bcf0a7787c4d39079f9e
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-10-16 14:13:33 +0800

    Add files via upload

---
+++ REACT/297. React Dev Tools/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./components/Card";
+import contacts from "./components/contacts";
+import Avatar from "./components/Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://avatars.cloudflare.steamstatic.com/8a35cc362371a17dd0ed0d9108e9668f8d581833_full.jpg" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        phone={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        phone={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        phone={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
