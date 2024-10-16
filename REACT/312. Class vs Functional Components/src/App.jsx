8348b71c8fdad9ba4e159fed38b7d25619005d27
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-10-16 14:20:34 +0800

    Add files via upload

---
+++ REACT/312. Class vs Functional Components/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ClassComponent from "./components/ClassComponent";
+import FunctionalComponent from "./components/FunctionalComponent";
+
+class App extends React.Component {
+  render() {
+    return <ClassComponent />;
+  }
+}
+
+export default App;
